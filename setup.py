986a5701e38f14389f4e699c579192d815966cb6
Jordan Matelsky <j6k4m8@gmail.com>
2015-09-30 10:24:09 -0400

    add setup scripts

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+    name = 'ndio',
+    packages = [
+        'ndio',
+        'ndio.convert',
+        'ndio.ramon',
+        'ndio.remote',
+        'ndio.utils'
+    ],
+    version = '0.0.1',
+    description = 'A Python library for neuroscience data access and manipulation.',
+    author = 'Jordan Matelsky',
+    author_email = 'jordan.matelsky@jhu.edu',
+    url = 'https://github.com/openconnectome/ndio',
+    download_url = 'https://github.com/openconnectome/ndio/tarball/0.0.1',
+    keywords = [
+        'brain',
+        'neuro',
+        'neurodata',
+        'neurodata.io',
+        'neuroscience',
+        'ocpy',
+        'ocp',
+        'ocp.me',
+        'connectome',
+        'connectomics'
+    ],
+    classifiers = [],
+)
