a63ddc44c49eafd2807dabcb30e48941bd73e92c
Jordan Matelsky <j6k4m8@gmail.com>
2016-01-11 11:03:44 -0500

    add docs to connected-components

--- ndio/utils/stats.py
+++ ndio/utils/stats.py
@@ -3,6 +3,10 @@ from scipy import ndimage
 
 
 def connected_components(data):
+    """
+    Run a jank implementation of connected-components on a bit of
+    data. Probably shouldn't live here but it does for now.
+    """
     mask = data > data.mean()
     label_im, nb_labels = ndimage.label(mask)
     return (label_im, nb_labels)
