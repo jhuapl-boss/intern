281e5e68b55c82bfab542fe0b5b5e9ad7c51d167
Alex Eusman <aeus@jhu.edu>
2016-01-12 15:34:16 -0500

    Pep8 compliance

--- ndio/utils/autoingest.py
+++ ndio/utils/autoingest.py
@@ -18,6 +18,7 @@ import requests
 import os
 import requests
 
+
 class AutoIngest:
 
     def __init__(self):
@@ -28,65 +29,75 @@ class AutoIngest:
 
     def add_channel(
         self, channel_name, datatype, channel_type, data_url, file_format,
-                    file_type, exceptions=0, resolution=0, windowrange=(0, 0),
-                    readonly=0):
+            file_type, exceptions=0, resolution=0,
+            windowrange=(0, 0), readonly=0):
         """
         Arguments:
-            channel_name (str): Channel Name is the specific name of a specific
-            series of data. Standard naming convention is to do
+            channel_name (str): Channel Name is the specific name of a
+            specific series of data. Standard naming convention is to do
             ImageTypeIterationNumber or NameSubProjectName.
 
-            datatype (str): The data type is the storage method of data in the
-            channel. It can be uint8, uint16, uint32, uint64, or float32.
+            datatype (str): The data type is the storage method of data in
+            the channel. It can be uint8, uint16, uint32, uint64, or
+            float32.
 
-            channel_type (str): The channel type is the kind of data being stored in
-            the channel. It can be image, annotation, or timeseries.
+            channel_type (str): The channel type is the kind of data being
+            stored in the channel. It can be image, annotation, or
+            timeseries.
 
-            data_url (str): This url points to the root directory of the files.
-            Dropbox (or any data requiring authentication to download such as s3) is
-            not an acceptable HTTP Server.
+            data_url (str): This url points to the root directory of the
+            files. Dropbox (or any data requiring authentication to
+            download such as s3) is not an acceptable HTTP Server.
 
-            file_format (str): File format refers to the overarching kind of data,
-            as in slices (normal image data) or catmaid (tile-based).
+            file_format (str): File format refers to the overarching kind
+            of data, as in slices (normal image data) or catmaid
+            (tile-based).
 
-            exceptions (int): Exceptions is an option to enable the possibility for
-            annotations to contradict each other (assign different values to the
-            same point). 1 corresponds to True, 0 corresponds to False.
+            file_type (str): File type refers to the specific type of file
+            that the data is stored in, as in, tiff, png, or tif.
 
-            resolution (int): Resolution is the starting resolution of the data
-            being uploaded to the channel.
+            exceptions (int): Exceptions is an option to enable the
+            possibility for annotations to contradict each other (assign
+            different values to the same point). 1 corresponds to True, 0
+            corresponds to False.
 
-            windowrange (int, int): Window range is the maximum and minimum pixel
-            values for a particular image. This is used so that the image can be
-            displayed in a readable way for viewing through RESTful calls
+            resolution (int): Resolution is the starting resolution of the
+            data being uploaded to the channel.
 
-            readonly (int): This option allows the user to control if, after the
-            initial data commit, the channel is read-only. Generally this is
-            suggested with data that will be publicly viewable.
+            windowrange (int, int): Window range is the maximum and minimum
+            pixel values for a particular image. This is used so that the
+            image can be displayed in a readable way for viewing through
+            RESTful calls
+
+            readonly (int): This option allows the user to control if,
+            after the initial data commit, the channel is read-only.
+            Generally this is suggested with data that will be publicly
+            viewable.
 
         Returns:
             None
         """
         self.channels[
             channel_name] = [channel_name, datatype, channel_type, data_url,
-                             file_format, file_type, exceptions, resolution, windowrange, readonly]
+                             file_format, file_type, exceptions, resolution,
+                             windowrange, readonly]
 
-    def add_project(self, project_name, token_name=None, public=0):
+    def add_project(self, project_name, token_name='', public=0):
         """
         Arguments:
-            project_name (str): Project name is the specific project within a
-            dataset’s name. If there is only one project associated with a dataset
-            then standard convention is to name the project the same as its
-            associated dataset.
+            project_name (str): Project name is the specific project within
+            a dataset’s name. If there is only one project associated with
+            a dataset then standard convention is to name the project the
+            same as its associated dataset.
 
-            token_name (str): The token name is the default token. If you do not
-            wish to specify one, a default one will be created for you with the same
-            name as the project name. However, if the project is private you must
-            specify a token.
+            token_name (str): The token name is the default token. If you
+            do not wish to specify one, a default one will be created for
+            you with the same name as the project name. However, if the
+            project is private you must specify a token.
 
-            public (int): This option allows users to specify if they want the
-            project/channels to be publicly viewable/search-able. (1, 0) = (TRUE,
-            FALSE)
+            public (int): This option allows users to specify if they want
+            the project/channels to be publicly viewable/search-able.
+            (1, 0) = (TRUE, FALSE)
 
         Returns:
             None
@@ -95,44 +106,48 @@ class AutoIngest:
         self.project = (project_name, token_name, public)
 
     def add_dataset(self, dataset_name, imagesize, voxelres,
-                    offset=(0, 0, 0), timerange=(0, 0), scalinglevels=0, scaling=0):
+                    offset=(0, 0, 0), timerange=(0, 0), scalinglevels=0,
+                    scaling=0):
         self.dataset = (dataset_name, imagesize, voxelres,
                         offset, timerange, scalinglevels, scaling)
         """
         Arguments:
-            dataset_name (str): Dataset Name is the overarching name of the research
-            effort. Standard naming convention is to do LabNamePublicationYear or
-            LeadResearcherCurrentYear.
+            dataset_name (str): Dataset Name is the overarching name of the
+            research effort. Standard naming convention is to do
+            LabNamePublicationYear or LeadResearcherCurrentYear.
 
-            imagesize (int, int, int): Image size is the pixel count dimensions of
-            the data. For example is the data is stored as a series of 100 slices
-            each 2100x2000 pixel TIFF images, the X,Y,Z dimensions are (2100, 2000,
-            100).
+            imagesize (int, int, int): Image size is the pixel count
+            dimensions of the data. For example is the data is stored as a
+            series of 100 slices each 2100x2000 pixel TIFF images, the X,Y,Z
+            dimensions are (2100, 2000, 100).
 
-            voxelres (flt, flt, flt): Voxel Resolution is the number of voxels per
-            unit pixel. We store X,Y,Z voxel resolution separately.
+            voxelres (flt, flt, flt): Voxel Resolution is the number of
+            voxels per unit pixel. We store X,Y,Z voxel resolution separately.
 
-            offset (int, int, int): If your data is not well aligned and there is
-            “excess” image data you do not wish to examine, but are present in your
-            images, offset is how you specify where your actual image starts. Offset
-            is provided a pixel coordinate offset from origin which specifies the
-            “actual” origin of the image. The offset is for X,Y,Z dimensions.
+            offset (int, int, int): If your data is not well aligned and
+            there is “excess” image data you do not wish to examine, but
+            are present in your images, offset is how you specify where your
+            actual image starts. Offset is provided a pixel coordinate offset
+            from origin which specifies the “actual” origin of the image.
+            The offset is for X,Y,Z dimensions.
 
-            timerange (int, int): Time Range is a parameter to support storage of
-            Time Series data, so the value of the tuple is a 0 to X range of how
-            many images over time were taken. It takes 2 inputs timeStepStart and
-            timeStepStop.
+            timerange (int, int): Time Range is a parameter to support
+            storage of Time Series data, so the value of the tuple is a 0
+            to X range of how many images over time were taken. It takes 2
+            inputs timeStepStart and timeStepStop.
 
-            scalinglevels (int): Scaling levels is the number of levels the data is
-            scalable to (how many zoom levels are present in the data). The highest
-            resolution of the data is at scaling level 0, and for each level up the
-            data is down sampled by 2x2 (per slice). To learn more about the
-            sampling service used, visit the the propagation service page.
+            scalinglevels (int): Scaling levels is the number of levels the
+            data is scalable to (how many zoom levels are present in the
+            data). The highest resolution of the data is at scaling level 0,
+            and for each level up the data is down sampled by 2x2
+            (per slice). To learn more about the sampling service used,
+            visit the the propagation service page.
 
-            scaling (int): Scaling is the orientation of the data being stored, 0
-            corresponds to a Z-slice orientation (as in a collection of tiff images
-            in which each tiff is a slice on the z plane) and 1 corresponds to an
-            isotropic orientation (in which each tiff is a slice on the y plane).
+            scaling (int): Scaling is the orientation of the data being
+            stored, 0 corresponds to a Z-slice orientation (as in a
+            collection of tiff images in which each tiff is a slice on the
+            z plane) and 1 corresponds to an isotropic orientation (in
+            which each tiff is a slice on the y plane).
 
         Returns:
             None
@@ -161,9 +176,9 @@ class AutoIngest:
 
         return json.dumps(ocp_dict, sort_keys=True, indent=4)
 
-    def dataset_dict(self, dataset_name, imagesize, voxelres,
-                     offset=[0, 0, 0], timerange=[0, 0], scalinglevels=0,
-                     scaling=0):
+    def dataset_dict(
+        self, dataset_name, imagesize, voxelres,
+            offset=[0, 0, 0], timerange=[0, 0], scalinglevels=0, scaling=0):
         """Generate the dataset dictionary"""
         dataset_dict = {}
         dataset_dict['dataset_name'] = dataset_name
@@ -181,8 +196,8 @@ class AutoIngest:
 
     def channel_dict(
         self, channel_name, datatype, channel_type, data_url, file_format,
-                     file_type, exceptions=0, resolution=0, windowrange=[0, 0],
-                     readonly=0):
+            file_type, exceptions=0, resolution=0,
+            windowrange=[0, 0], readonly=0):
         """Genearte the project dictionary"""
         channel_dict = {}
         channel_dict['channel_name'] = channel_name
@@ -206,8 +221,8 @@ class AutoIngest:
         project_dict = {}
         project_dict['project_name'] = project_name
         if token_name is not None:
-            project_dict[
-                'token_name'] = project_name if token_name == '' else token_name
+            project_dict['token_name'] = project_name \
+                if token_name == '' else token_name
         if public is not None:
             project_dict['public'] = public
         return project_dict
@@ -222,13 +237,15 @@ class AutoIngest:
         channel_names = data["channels"].keys()
 
         for i in range(0, len(channel_names)):
-            channel_type = data["channels"][channel_names[i]]["channel_type"]
+            channel_type = data["channels"][
+                channel_names[i]]["channel_type"]
             path = data["channels"][channel_names[i]]["data_url"]
 
             if (channel_type == "timeseries"):
                 timerange = data["dataset"]["timerange"]
                 for j in xrange(timerange[0], timerange[1] + 1):
-                    # Test for tifs or such? Currently test for just not empty
+                    # Test for tifs or such? Currently test for just not
+                    # empty
                     work_path = "{}{}/{}/time{}/".format(
                         path, token_name, channel_names[i], j)
                     resp = requests.head(work_path)
@@ -247,7 +264,7 @@ class AutoIngest:
         try:
             r = requests.post(URLPath, data=data)
         except:
-            print "Error in accessing JSON file"
+            print "Error in posting JSON file"
 
     def post_data(self, site_host):
         """
