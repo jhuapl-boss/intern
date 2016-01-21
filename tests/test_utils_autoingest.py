2c3273558d7c88f8d88452f820e4580827e23934
aeusman <Alex.Eusman@gmail.com>
2016-01-21 17:32:23 -0500

    Revert "Added Date/Time for testing"
    This reverts commit 78f5cc397b7fe1dc8db7c0f65446b3b89d6e6a23.

--- tests/test_utils_autoingest.py
+++ tests/test_utils_autoingest.py
@@ -5,29 +5,27 @@ import ndio.utils.autoingest as AutoIngest
 import numpy
 import datetime
 
-SERVER_SITE = 'http://http://ec2-54-200-94-232.us-west-2.compute.amazonaws.com/'
+SERVER_SITE = 'http://ec2-54-200-200-225.us-west-2.compute.amazonaws.com/'
 DATA_SITE = 'http://ec2-54-200-215-161.us-west-2.compute.amazonaws.com/'
 
 class TestAutoIngest(unittest.TestCase):
 
     def setUp(self):
         self.oo = OCP(SERVER_SITE)
-        self.i = datetime.datetime.now()
 
 
     def test_pull_data(self):
-        data_name_1 = "ndio_test_1_%s:%s:%s:%s:%s:%s" % (self.i.day, self.i.month, self.i.year, self.i.hour, self.i.month, self.i.second)
 
         ai_1 = AutoIngest.AutoIngest()
-        ai_1.add_channel(data_name_1, 'uint32', 'image',
+        ai_1.add_channel('ndio_test_1', 'uint32', 'image',
                     DATA_SITE, 'SLICE', 'tif')
 
-        ai_1.add_project(data_name_1, data_name_1, 1)
-        ai_1.add_dataset(data_name_1, (660, 528, 1), (0, 0, 0))
+        ai_1.add_project('ndio_test_1', 'ndio_test_1', 1)
+        ai_1.add_dataset('ndio_test_1', (660, 528, 1), (0, 0, 0))
         ai_1.add_metadata('')
 
         ai_1.post_data(SERVER_SITE)
-        numpy_download = self.oo.get_cutout(data_name_1, data_name_1,
+        numpy_download = self.oo.get_cutout('ndio_test_1', 'ndio_test_1',
                                             0, 660,
                                             0, 528,
                                             0, 0,
@@ -35,26 +33,24 @@ class TestAutoIngest(unittest.TestCase):
 
         self.assertEqual(type(numpy_download), numpy.ndarray)
         #Verify its the same image?
-        self.oo.delete_channel(data_name_1, data_name_1)
+        self.oo.delete_channel('ndio_test_1', 'ndio_test_1')
 
 
     def test_post_json(self):
-        data_name_2 = "ndio_test_2_%s:%s:%s:%s:%s:%s" % (self.i.day, self.i.month, self.i.year, self.i.hour, self.i.month, self.i.second)
-
         ai_2 = AutoIngest.AutoIngest()
-        ai_2.add_channel(data_name_2, 'uint32', 'image',
+        ai_2.add_channel('ndio_test_2', 'uint32', 'image',
                     DATA_SITE, 'SLICE', 'tif')
 
-        ai_2.add_project(data_name_2, data_name_2, 1)
-        ai_2.add_dataset(data_name_2, (660, 528, 1), (0, 0, 0))
+        ai_2.add_project('ndio_test_2', 'ndio_test_2', 1)
+        ai_2.add_dataset('ndio_test_2', (660, 528, 1), (0, 0, 0))
         ai_2.add_metadata('')
-
+ 
         ai_2.output_json()
-
+ 
         ai_3 = AutoIngest.AutoIngest()
         ai_3.post_data(SERVER_SITE, "/tmp/ND.json")
 
-        numpy_download = self.oo.get_cutout(data_name_2, data_name_2,
+        numpy_download = self.oo.get_cutout('ndio_test_2', 'ndio_test_2',
                                             0, 660,
                                             0, 528,
                                             0, 0,
@@ -62,7 +58,7 @@ class TestAutoIngest(unittest.TestCase):
 
         self.assertEqual(type(numpy_download), numpy.ndarray)
 
-        self.oo.delete_channel(data_name_2, data_name_2)
+        self.oo.delete_channel('ndio_test_2', 'ndio_test_2')
 
 if __name__ == '__main__':
     unittest.main()
