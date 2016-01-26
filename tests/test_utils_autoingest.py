6360e6c10312c70a0fd55efdce4bb6cbc30604fa
aeusman <Alex.Eusman@gmail.com>
2016-01-26 15:04:43 -0500

    More verbose error

--- tests/test_utils_autoingest.py
+++ tests/test_utils_autoingest.py
@@ -14,7 +14,6 @@ class TestAutoIngest(unittest.TestCase):
 
     def test_pull_data(self):
         data_name_1 = "ndiotest1%s%s%s%s%s" % (self.i.year, self.i.month, self.i.day, self.i.hour, self.i.second)
-        #data_name_1 = "ndio_test_1"
 
         ai_1 = AutoIngest.AutoIngest()
         ai_1.add_channel(data_name_1, 'uint32', 'image', DATA_SITE, 'SLICE', 'tif')
@@ -24,17 +23,18 @@ class TestAutoIngest(unittest.TestCase):
 
         response = requests.get("{}/ocp/ca/{}/{}/npz/0/0,660/0,528/0,1/".format(SERVER_SITE,data_name_1, data_name_1))
 
-        self.assertEqual(response.headers['content-type'], 'product/npz')
-        #Verify its the same image?
+        try:
+            self.assertEqual(response.headers['content-type'], 'product/npz')
+        except:
+            print(response.content)
+            print("{}/ocp/ca/{}/{}/npz/0/0,660/0,528/0,1/".format(SERVER_SITE,data_name_1, data_name_1))
 
 
     def test_post_json(self):
         data_name_2 = "ndiotest2%s%s%s%s%s" % (self.i.year, self.i.month, self.i.day, self.i.hour, self.i.second)
-        #data_name_2 = "ndio_test_2"
 
         ai_2 = AutoIngest.AutoIngest()
-        ai_2.add_channel(data_name_2, 'uint32', 'image',
-                    DATA_SITE, 'SLICE', 'tif')
+        ai_2.add_channel(data_name_2, 'uint32', 'image', DATA_SITE, 'SLICE', 'tif')
 
         ai_2.add_project(data_name_2, data_name_2, 1)
         ai_2.add_dataset(data_name_2, (660, 528, 1), (1.0, 1.0, 1.0))
@@ -47,7 +47,11 @@ class TestAutoIngest(unittest.TestCase):
 
         response = requests.get("{}/ocp/ca/{}/{}/npz/0/0,660/0,528/0,1/".format(SERVER_SITE,data_name_2, data_name_2))
 
-        self.assertEqual(response.headers['content-type'], 'product/npz')
+        try:
+            self.assertEqual(response.headers['content-type'], 'product/npz')
+        except:
+            print(response.content)
+            print("{}/ocp/ca/{}/{}/npz/0/0,660/0,528/0,1/".format(SERVER_SITE,data_name_2, data_name_2))
 
 
 if __name__ == '__main__':
