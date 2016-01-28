675c68c0d0ac4d25e5f1619f348a9531724a72c1
aeusman <Alex.Eusman@gmail.com>
2016-01-28 17:53:31 -0500

    No Errors Locally, replicating

--- tests/test_utils_autoingest.py
+++ tests/test_utils_autoingest.py
@@ -66,7 +66,7 @@ class TestAutoIngest(unittest.TestCase):
         data_name_2 = "ndiotest2%s%s%s%s%sf" % (self.i.year, self.i.month, self.i.day, self.i.hour, self.i.second)
 
         ai_2 = AutoIngest.AutoIngest()
-        ai_2.add_channel(data_name_2, 'uint8', 'image', DATA_SITE, 'SLICE', 'tif')
+        ai_2.add_channel(data_name_2, 'uint8', 'image', DATA_SITE, 'SLICE', 'tif') 
 
         ai_2.add_project(data_name_2, data_name_2, 1)
         ai_2.add_dataset(data_name_2, (512, 512, 1), (1.0, 1.0, 1.0))
