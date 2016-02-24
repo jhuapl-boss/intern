347444b364ef2af4fc6f24ca8f7622801b33efce
Jordan Matelsky <j6k4m8@gmail.com>
2016-02-24 18:47:38 -0500

    add xyzzyx tests

---
+++ tests/test_xyzzyx.py
@@ -0,0 +1,27 @@
+import unittest
+import ndio.remote.neurodata as neurodata
+import ndio.remote.errors
+import numpy
+import h5py
+import os
+
+
+class TestXYZZYX(unittest.TestCase):
+
+    def setUp(self):
+        self.nd = neurodata()
+
+    def test_post_get(self):
+        token = 'ndio_demos'
+        channel = 'image'
+        cutout = numpy.ones((5, 5, 5)).astype(int)
+        cutout[2,3,4] = 42
+        self.nd.post_cutout(token, channel, 20, 20, 20, cutout, resolution=0)
+        pulldown = self.nd.get_cutout(token, channel, 20, 25, 20, 25, 20, 25, resolution=0)
+        import pdb; pdb.set_trace()
+        
+        self.assertEqual(cutout[2,3,4], pulldown[2,3,4])
+
+
+if __name__ == '__main__':
+    unittest.main()
