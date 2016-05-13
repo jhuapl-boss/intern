9491939e85fac763d05256af6b3f922f49597e92
Tim Gion <movestill@gmail.com>
2016-05-13 13:26:16 -0400

    Metadata service usage example.

---
+++ examples/metadata_ex.py
@@ -0,0 +1,97 @@
+# Copyright 2016 The Johns Hopkins University Applied Physics Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This example shows how to manage metadata with the Boss API.  The Remote
+class methods that being with 'metadata_' manipulate metadata.  These
+methods are:
+    metadata_list()
+    metadata_create()
+    metadata_get()
+    metadata_update()
+    metadata_delete()
+
+All of these methods take a ndio.ndresource.boss.resource.Resource object, as
+a minimum.  The resource object identifies which data model object's metadata
+will be manipulated.
+"""
+
+from ndio.remote.boss.remote import Remote
+from ndio.ndresource.boss.resource import *
+from requests import HTTPError
+import sys
+
+rmt = Remote('test.cfg')
+
+API_VER = 'v0.4'
+
+# Turn off SSL cert verification.  This is necessary for interacting with
+# developer instances of the Boss.
+import requests
+from requests import Session
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+rmt.project_service.session_send_opts = { 'verify': False }
+rmt.metadata_service.session_send_opts = { 'verify': False }
+rmt.volume_service.session_send_opts = { 'verify': False }
+
+# First, create resource objects that identify the objects of interest in the
+# data model.  Notice, that only the minimal information needed to identify
+# the objects of interest is given to the resources' constructors.
+coll = CollectionResource('gray', API_VER)
+alpha_exp = ExperimentResource('alpha', 'gray', API_VER)
+omega_chan = ChannelResource('omega', 'gray', 'alpha', API_VER)
+
+# Add new metadata using metadata_create().
+if not rmt.metadata_create(coll, { 'mark': 'two', 'ten': 'four'}):
+    print('Failed to create metadata for collection.')
+if not rmt.metadata_create(alpha_exp, { 'date': '04May2016', 'time': '13:00' }):
+    print('Failed to create metadata for experiment.')
+if not rmt.metadata_create(
+    omega_chan, 
+    { 'algorithm': 'indigo', 'iterations': '1000', 'confidence': '75' }
+):
+    print('Failed to create metadata for channel.')
+
+# Retrieve metadata with metadata_get().
+# Use a list with a single string if you only want a single value.
+mark = rmt.metadata_get(coll, ['mark'])
+print(mark['mark'])
+omega_metadata = rmt.metadata_get(omega_chan, ['algorithm', 'iterations', 'confidence'])
+for pair in omega_metadata.items():
+    print('{}: {}'.format(pair[0], pair[1]))
+
+# List existing metadata keys using metadata_list().
+omega_list = rmt.metadata_list(omega_chan)
+for k in omega_list:
+    print(k)
+
+# Update metadata using metadata_update().
+if not rmt.metadata_update(omega_chan, {'iterations': '2000', 'confidence': '80'}):
+    print('Failed updating metadata for channel.')
+
+# Confirm updated values.
+omega_metadata = rmt.metadata_get(omega_chan, ['iterations', 'confidence'])
+print(omega_metadata['iterations'])
+print(omega_metadata['confidence'])
+
+# Use metadata_delete() to remove keys and values.
+if not rmt.metadata_delete(alpha_exp, ['date', 'time']):
+    print('Failed to delete metadata from experiment')
+if not rmt.metadata_delete(coll, ['mark', 'ten']):
+    print('Failed to delete metadata for collection.')
+if not rmt.metadata_delete(
+    omega_chan, ['algorithm', 'iterations', 'confidence']
+):
+    print('Failed to delete metadata for channel.')
