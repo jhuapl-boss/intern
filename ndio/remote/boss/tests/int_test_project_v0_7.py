9a9c002b98224b96b8a42701b7694b07bb30337f
Tim Gion <movestill@gmail.com>
2016-10-25 16:59:12 -0400

    Added random num to coord frame/collection names. Randomized to avoid collisions with simultaneous integration test runs.

--- ndio/remote/boss/tests/int_test_project_v0_7.py
+++ ndio/remote/boss/tests/int_test_project_v0_7.py
@@ -15,6 +15,7 @@
 from ndio.remote.boss import BossRemote
 from ndio.ndresource.boss.resource import *
 
+import random
 import requests
 from requests import Session, HTTPError
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -52,11 +53,14 @@ class ProjectServiceTest_v0_7(unittest.TestCase):
         self.rmt.volume_service.session_send_opts = { 'verify': False }
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-        self.coll = CollectionResource('collection2309', API_VER, 'bar')
+        coll_name = 'collection2309-{}'.format(random.randint(0, 9999))
+        self.coll = CollectionResource(coll_name, API_VER, 'bar')
+        coll_name_upd = '{}-{}'.format(coll_name, random.randint(0, 9999))
         self.coll_upd = CollectionResource('collection2310', API_VER, 'latest')
 
+        cf_name = 'BestFrame{}'.format(random.randint(0, 9999))
         self.coord = CoordinateFrameResource(
-            'BestFrame', API_VER, 'Test coordinate frame.', 0, 10, -5, 5, 3, 6, 
+            cf_name, API_VER, 'Test coordinate frame.', 0, 10, -5, 5, 3, 6, 
             1, 1, 1, 'nanometers', 2, 'nanoseconds')
         self.coord_upd = copy.copy(self.coord)
         self.coord_upd.name = 'MouseFrame'
