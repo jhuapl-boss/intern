495c6ded7416488b48c05c3d2605aa6b8f0dc48e
Luis Rodriguez <lrodri729@gmail.com>
2018-04-09 23:37:52 -0400

    mounts sock

--- xbrainDock/dvidInit.py
+++ xbrainDock/dvidInit.py
@@ -2,6 +2,6 @@
 imagePath = "./publicData/proj4"
 
 call(["docker", "pull", "flyem/dvid"])
-call(["docker", "run", "-v", imagePath + ":/dataLoad/", "--name", "xbrain_repr", "-d", "flyem/dvid"])
+call(["docker", "run", "-v", imagePath + ":/dataLoad/", "/var/run/docker.sock:/var/run/docker.sock" ,"--name", "xbrain_repr", "-d", "flyem/dvid"])
 print("Running your container...")
 call(["docker", "run", "--volumes-from", "xbrain_repr", "-p", "8000" + ":" + "8000", "--name", "xbrain_port1", "-t", "flyem/dvid"])
