b96e72933d36dd40f3236fd27c12edae1c0c1713
Daniel Xenes <dxenes1@jhu.edu>
2019-04-25 17:22:34 -0400

    fixed gcp_test w/ instance resource methods

--- cloudvolume_test/gcp_test.py
+++ cloudvolume_test/gcp_test.py
@@ -1,15 +1,15 @@
 import os
-import resource as cvr
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-cv_resource = cvr.CloudVolumeResource()
+from intern.resource.cv.resource import *
+from intern.service.cv.service import *
+
 
 gcp_path = '/kasthuri2015/test_dataset/em'
-description = 'test with google cloud service'
 
-vol = cv_resource.create_CV('gs', gcp_path, description, 
+vol = CloudVolumeResource('gs', gcp_path, 
 	num_channels = 1, 
 	data_type = 'uint8',
 	layer_type = 'image',
@@ -17,7 +17,8 @@ vol = cv_resource.create_CV('gs', gcp_path, description,
 	resolution = [1,1,1],
 	voxel_offset = [0,0,0],
 	chunk_size = [1100,700,1],
-	volume_size = [1100,700,10])
+	volume_size = [1100,700,10],
+	description = 'test with google cloud service')
 
 for z in range(10):
 	path = 'test_dataset/'
@@ -26,11 +27,14 @@ for z in range(10):
 	width, height = image.size
 	IMarray = np.array(list(image.getdata()), dtype=np.uint8)
 	IMarray = IMarray.reshape((1, height, width)).T
-	cv_resource.create_cutout(IMarray, vol, [0, width], [0, height], [z,z+1])
+	vol.create_cutout(IMarray, [0, width], [0, height], [z,z+1])
 	image.close()
 
-cutout = cv_resource.get_cutout(vol, [0,100], [0,100], [0,10])
+cutout = vol.get_cutout([0,100], [0,100], [0,10])
+
 
 for z in range(10):
 	im = Image.fromarray(cutout[:,:,z,0])
 	im.save('cutout_{}.png'.format(z)) 
+
+print(CloudVolumeService.get_info(vol))
