348d11f76da54379a22d736cb1475a6c287ca83f
Daniel Xenes <daniel.xenes@jhuapl.edu>
2020-01-10 16:51:09 -0500

    updates get_extents

--- intern/service/cv/metadata.py
+++ intern/service/cv/metadata.py
@@ -14,150 +14,167 @@
 
 from intern.service.cv import CloudVolumeService
 from intern.resource.cv.resource import *
-
+import json
 
 class MetadataService(CloudVolumeService):
-	"""
-	MetadataService for cloud-volume service.
-	"""
+    """
+    MetadataService for cloud-volume service.
+    """
 
-	def __init__(self):
-		"""
-		Constructor.
-		"""
-		CloudVolumeService.__init__(self)	
-	
-	def get_info(self, resource):
-		"""
-		Returns a JSON of the resource properties.
+    def __init__(self):
+        """
+        Constructor.
+        """
+        CloudVolumeService.__init__(self)	
+    
+    def get_info(self, resource):
+        """
+        Returns a JSON of the resource properties.
 
-		Args:
-		resource (CloudVolumeResource object)
+        Args:
+        resource (CloudVolumeResource object)
 
-		Returns:
-		string: JSON format of properties
-		"""
-		return resource.cloudvolume.info
+        Returns:
+        string: JSON format of properties
+        """
+        return resource.cloudvolume.info
 
-	def get_cloudpath(self, resource):
-		"""
-		Returns a string of the cloudpath of the resource.
+    def get_cloudpath(self, resource):
+        """
+        Returns a string of the cloudpath of the resource.
 
-		Args:
-		resource (CloudVolumeResource object)
+        Args:
+        resource (CloudVolumeResource object)
 
-		Returns:
-		string: in the form of 'PROTOCOL//:BUCKET/../DATASET/LAYER'
-		"""
-		return resource.cloudvolume.layer_cloudpath
+        Returns:
+        string: in the form of 'PROTOCOL//:BUCKET/../DATASET/LAYER'
+        """
+        return resource.cloudvolume.layer_cloudpath
 
-	def set_provenance(self, resource, **kwargs):
-		"""
-		Sets the description and owners of the cloudvolume resource.
+    def set_provenance(self, resource, **kwargs):
+        """
+        Sets the description and owners of the cloudvolume resource.
 
-		Args:
-		resource (CloudVolumeResource object)
-		owners (list): list of authorship names and emails
-		description (str): description of dataset
+        Args:
+        resource (CloudVolumeResource object)
+        owners (list): list of authorship names and emails
+        description (str): description of dataset
 
-		Returns:
-			None
-		"""
-		if kwargs.get('owners') != None:
-			resource.cloudvolume = kwargs.get('owners')
-		if kwargs.get('description') != None:
-			resource.cloudvolume = kwargs.get('description')
-		resource.cloudvolume.commit_provenance()
-	
-	def get_provenance(self, resource):
-		"""
-		Returns the description and owners of the cloudvolume resource.
+        Returns:
+            None
+        """
+        if kwargs.get('owners') != None:
+            resource.cloudvolume = kwargs.get('owners')
+        if kwargs.get('description') != None:
+            resource.cloudvolume = kwargs.get('description')
+        resource.cloudvolume.commit_provenance()
+    
+    def get_provenance(self, resource):
+        """
+        Returns the description and owners of the cloudvolume resource.
 
-		Args:
-		resource (CloudVolumeResource object)
+        Args:
+        resource (CloudVolumeResource object)
 
-		Returns:
-		dict: desciption and owners values
-		"""
-		return resource.cloudvolume.refresh_provenance()
+        Returns:
+        dict: desciption and owners values
+        """
+        return resource.cloudvolume.refresh_provenance()
 
-	def _chunks_exist(self, resource, x_range, y_range, z_range):
-		"""
-		Get a report on which chunks actually exist.
+    def _chunks_exist(self, resource, x_range, y_range, z_range):
+        """
+        Get a report on which chunks actually exist.
 
-		Args:
-		resource (CloudVolumeResource object)
-		x_range,y_range,z_range (Tuples representing the bbox)
+        Args:
+        resource (CloudVolumeResource object)
+        x_range,y_range,z_range (Tuples representing the bbox)
 
-		Returns: {chunk_file_name: boolean, ...}
+        Returns: {chunk_file_name: boolean, ...}
 
-		"""
-		x1,x2 = x_range
-		y1,y2 = y_range
-		z1,z2 = z_range
-		return resource.cloudvolume.exists( np.s_[x1:x2, y1:y2, z1:z2] ) 
+        """
+        x1,x2 = x_range
+        y1,y2 = y_range
+        z1,z2 = z_range
+        return resource.cloudvolume.exists( np.s_[x1:x2, y1:y2, z1:z2] ) 
 
-	def list_res(self, resource):
-		"""
-		What resolution(s) are available to read and write to in the current resource. 
+    def list_res(self, resource):
+        """
+        What resolution(s) are available to read and write to in the current resource. 
 
-		Args:
-		resource (CloudVolume Resource Object)
+        Args:
+        resource (CloudVolume Resource Object)
 
-		Returns: (list) list of ints denoting mip levels
-		"""
-		return resource.cloudvolume.available_mips
+        Returns: (list) list of ints denoting mip levels
+        """
+        return resource.cloudvolume.available_mips
 
-	def get_layer(self, resource):
-		"""
-		Which data layer (e.g. image, segmentation) on S3, GS, or FS you're reading and writing to. 
-		Known as a "channel" in BOSS terminology.
-		
-		Args:
-		resource (CloudVolume Resource Object)
+    def get_layer(self, resource):
+        """
+        Which data layer (e.g. image, segmentation) on S3, GS, or FS you're reading and writing to. 
+        Known as a "channel" in BOSS terminology.
+        
+        Args:
+        resource (CloudVolume Resource Object)
 
-		Returns:
-		str: the resource channel 
+        Returns:
+        str: the resource channel 
 
-		"""	
-		return resource.cloudvolume.layer
-	
-	def set_layer(self, resource, layer):
-		"""
-		Set a new layer and commits it to the info file.
-		
-		Args:
-		resource (CloudVolume Resource Object)
-		layer (string)
+        """	
+        return resource.cloudvolume.layer
+    
+    def set_layer(self, resource, layer):
+        """
+        Set a new layer and commits it to the info file.
+        
+        Args:
+        resource (CloudVolume Resource Object)
+        layer (string)
 
-		Returns:
-		None
-		"""
-		resource.cloudvolume.layer = str(layer)
+        Returns:
+        None
+        """
+        resource.cloudvolume.layer = str(layer)
 
-	def get_dataset_name(self, resource):
-		"""
-		Which dataset (e.g. test_v0, snemi3d_v0) on S3, GS, or FS you're reading and writing to. 
-		Known as an "experiment" in BOSS terminology. Writing to this property triggers an info refresh.
-		
-		Args: 
-		resource (CloudVolume Resource Object) 
+    def get_dataset_name(self, resource):
+        """
+        Which dataset (e.g. test_v0, snemi3d_v0) on S3, GS, or FS you're reading and writing to. 
+        Known as an "experiment" in BOSS terminology. Writing to this property triggers an info refresh.
+        
+        Args: 
+        resource (CloudVolume Resource Object) 
 
-		Returns:
-		str: current resource experiment
-		"""
-		return resource.cloudvolume.dataset_name
+        Returns:
+        str: current resource experiment
+        """
+        return resource.cloudvolume.dataset_name
 
-	def set_dataset_name(self, resource, experiment):
-		"""
-		Which dataset (e.g. test_v0, snemi3d_v0) on S3, GS, or FS you're reading and writing to. 
-		Known as an "experiment" in BOSS terminology. Writing to this property triggers an info refresh.
-		
-		Args: 
-		resource (CloudVolume Resource Object)
-		experiment (str): experiment name 
+    def set_dataset_name(self, resource, experiment):
+        """
+        Which dataset (e.g. test_v0, snemi3d_v0) on S3, GS, or FS you're reading and writing to. 
+        Known as an "experiment" in BOSS terminology. Writing to this property triggers an info refresh.
+        
+        Args: 
+        resource (CloudVolume Resource Object)
+        experiment (str): experiment name 
 
-		Returns:
-		None
-		"""
-		resource.cloudvolume.dataset_name = str(experiment)
+        Returns:
+        None
+        """
+        resource.cloudvolume.dataset_name = str(experiment)
+    
+    def get_extents(self, resource):
+        """Gets extents to a specific cloudvolume resource
+        Args:
+            resource : cloudvolume resource to which to relate metadata
+        Returns:
+             extents (array): [[x-min, x-max], [y-min, y-max], [z-min, z-max]]
+        """
+        info_json = json.loads(resource.cloudvolume.info)
+        min_point = info_json["voxel_offset"]
+        max_point = info_json["volume_size"]
+        extents = [
+            [min_point[0], max_point[0]],
+            [min_point[1], max_point[1]], 
+            [min_point[2], max_point[2]]
+        ]
+        return extents
