623f55768653d5f07e34926807ca27a168213cbb
Daniel Xenes <daniel.xenes@jhuapl.edu>
2020-01-21 14:02:50 -0500

    adds delete and mip tests

--- examples/cloudvolume/test_local_remote.py
+++ examples/cloudvolume/test_local_remote.py
@@ -77,6 +77,38 @@ class TestCloudVolumeRemote(unittest.TestCase):
         cutout = self.cv_remote.get_cutout(resource, 0, [32,96], [32,96], [32,96])
         np.testing.assert_array_equal(data[32:96, 32:96, 32:96], cutout)
     
+    def test_multiple_mips(self):
+        # Create Info JSON
+        info = self.cv_remote.create_new_info(
+            num_channels=1,
+            layer_type="image",
+            data_type="uint8",
+            resolution=(10,10,10),
+            volume_size=(128,128,128),
+            chunk_size=(32,32,32),
+            max_mip=1,
+            factor = (2,2,1)
+            )
+        
+        # Instantiate a new cloudvolume resource
+        resource = self.cv_remote.cloudvolume(info=info)
+        
+        # Upload data to mip 0
+        data_0 = np.random.randint(0, 255, [128,128,128], dtype=np.uint8)
+        self.cv_remote.create_cutout(resource, 0, [0,128], [0,128], [0,128], data_0)
+
+        # Upload data to mip 1
+        data_1 = np.random.randint(0, 255, [64,64,128], dtype=np.uint8)
+        self.cv_remote.create_cutout(resource, 1, [0,64], [0,64], [0,128], data_1)
+
+        # Download cutout and check for equivalence 
+        cutout_0 = self.cv_remote.get_cutout(resource, 0, [0,128], [0,128], [0,128])
+        np.testing.assert_array_equal(data_0, cutout_0)
+        
+        # Download cutout and check for equivalence 
+        cutout_1 = self.cv_remote.get_cutout(resource, 1, [0,64], [0,64], [0,128])
+        np.testing.assert_array_equal(data_1, cutout_1)         
+    
     def test_metadata(self):
         # Create Info JSON
         info = self.cv_remote.create_new_info(
@@ -85,7 +117,8 @@ class TestCloudVolumeRemote(unittest.TestCase):
             data_type="uint8",
             resolution=(10,10,10),
             volume_size=(128,128,128),
-            chunk_size=(32,32,32)
+            chunk_size=(32,32,32),
+            max_mip=7
             )
         
         # Instantiate a new cloudvolume resource
@@ -99,8 +132,33 @@ class TestCloudVolumeRemote(unittest.TestCase):
             )
 
         self.assertEqual(info, self.cv_remote.get_info(resource))
+        self.assertEqual("file://" + os.path.join(os.getcwd(),"test_data/images"), self.cv_remote.get_cloudpath(resource))
         self.assertEqual("images", self.cv_remote.get_layer(resource))
         self.assertEqual("test_data", self.cv_remote.get_dataset_name(resource))
+        self.assertEqual(range(0,8), self.cv_remote.list_res(resource))
+    
+    def test_delete(self):
+         # Create Info JSON
+        info = self.cv_remote.create_new_info(
+            num_channels=1,
+            layer_type="image",
+            data_type="uint8",
+            resolution=(10,10,10),
+            volume_size=(128,128,128),
+            chunk_size=(32,32,32)
+            )
+        
+        # Instantiate a new cloudvolume resource
+        resource = self.cv_remote.cloudvolume(info=info, fill_missing=True)
+        # Upload data
+        data = np.random.randint(0, 255, [128,128,128], dtype=np.uint8)
+        self.cv_remote.create_cutout(resource, 0, [0,128], [0,128], [0,128], data)
+        
+        # Delete data
+        self.cv_remote.delete_data(resource, 0, [0,64], [0,64], [0,64])
+
+        np.testing.assert_array_equal(np.zeros([64,64,64]), self.cv_remote.get_cutout(resource, 0, [0,64], [0,64], [0,64]))
+
 
     def tearDown(self):
         shutil.rmtree(FILE_PATH)
