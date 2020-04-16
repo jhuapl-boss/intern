70d8e203aff280264f64bbcfd6fe9db87cb32d23
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2020-04-16 14:40:07 -0400

    Prepare for 0.10.0 release (#49)

--- intern/__init__.py
+++ intern/__init__.py
@@ -2,7 +2,7 @@
 A Python library for open neuroscience data access and manipulation.
 """
 
-__version__ = "0.9.12"
+__version__ = "0.10.0"
 
 
 def check_version():
@@ -10,9 +10,14 @@ def check_version():
     Tells you if you have an old version of intern.
     """
     import requests
-    r = requests.get('https://pypi.python.org/pypi/intern/json').json()
-    r = r['info']['version']
+
+    r = requests.get("https://pypi.python.org/pypi/intern/json").json()
+    r = r["info"]["version"]
     if r != __version__:
-        print("You are using version {}. A newer version of intern is available: {} ".format(__version__, r) +
-              "\n\n'pip install -U intern' to update.")
+        print(
+            "You are using version {}. A newer version of intern is available: {} ".format(
+                __version__, r
+            )
+            + "\n\n'pip install -U intern' to update."
+        )
     return r
