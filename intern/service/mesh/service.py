fix tuple units

intern/service/mesh/service.py
@@ -16,19 +16,17 @@ from intern.service.service import Service
 from enum import IntEnum
 import numpy as np
 
-
 class VoxelUnits(IntEnum):
     """Enum with valid VoxelUnits
     """
-
-    nm = (1,)
-    um = (1000,)
-    mm = (1000000,)
-    cm = (10000000,)
-    nanometers = (1,)
-    micrometers = (1000,)
-    millimeters = (1000000,)
-    centimeters = (10000000,)
+    nm = 1
+    um = 1000
+    mm = 1000000
+    cm = 10000000
+    nanometers = 1
+    micrometers = 1000
+    millimeters = 1000000
+    centimeters = 10000000
 
 
 class MeshService(Service):
@@ -46,21 +44,11 @@ class MeshService(Service):
 		"""
         self._auth = None
 
-    def create(
-        self,
-        volume,
-        x_range,
-        y_range,
-        z_range,
-        time_range=None,
-        id_list=[],
-        voxel_unit=VoxelUnits.nm,
-        voxel_size=[4, 4, 40],
-        simp_fact=0,
-        max_simplification_error=60,
-        normals=False,
-        **kwargs
-    ):
+    def create(self, volume,
+            x_range, y_range, z_range, time_range=None, 
+            id_list=[], voxel_unit=VoxelUnits.nm, 
+            voxel_size=[4,4,40], simp_fact=0, max_simplification_error=60,
+            normals=False, **kwargs):
         """Generate a mesh of the specified IDs
 
         Args:
@@ -71,7 +59,7 @@ class MeshService(Service):
             z_range (list[int]): z range such as [10, 20] which means z>=10 and z<20.
             time_range (optional [list[int]]): time range such as [30, 40] which means t>=30 and t<40.
             id_list (optional [list]): list of object ids to filter the volume by.
-            voxel_unit (optional VoxelUnit): voxel unit of measurement to derive conversion factor.
+            voxel_unit (optional VoxelUnit): voxel unit of measurement to derive conversion factor. 
             voxel_size (optional [list]): list in form [x,y,z] of voxel size. Defaults to 4x4x40nm
             simp_fact (optional int): mesh simplification factor, reduces triangles by given factor
             max_simplification_error (optional int): Max tolerable error in physical distance
@@ -86,41 +74,34 @@ class MeshService(Service):
 
         """
 
+        from zmesh import Mesher
         if np.unique(volume).shape[0] == 1:
-            raise ValueError(
-                "The volume provided only has one unique ID (0). ID 0 is considered background."
-            )
+            raise ValueError("The volume provided only has one unique ID (0). ID 0 is considered background.")
 
         conv_factor = self._get_conversion_factor(voxel_unit)
-
+        
         # Get voxel_sizes
         x_voxel_size = float(voxel_size[0]) * conv_factor
         y_voxel_size = float(voxel_size[1]) * conv_factor
         z_voxel_size = float(voxel_size[2]) * conv_factor
 
         # Mesh
-        from zmesh import Mesher
-
-        mesher = Mesher((x_voxel_size, y_voxel_size, z_voxel_size))
+        mesher = Mesher((x_voxel_size,y_voxel_size,z_voxel_size))
         mesher.mesh(volume)
 
         # If the list is empty then just default to all ID's found in the volume
-        if id_list == []:
+        if (id_list == []):
             id_list = mesher.ids()
 
         # Run the mesher on all specified ID's
         for oid in id_list:
             mesh = mesher.get_mesh(
-                oid,
-                normals=normals,
+                oid, 
+                normals=normals, 
                 simplification_factor=simp_fact,
-                max_simplification_error=max_simplification_error,
-            )
-            mesh.vertices += [
-                x_range[0] * conv_factor,
-                y_range[0] * conv_factor,
-                z_range[0] * conv_factor,
-            ]
+                max_simplification_error= max_simplification_error,
+                )
+            mesh.vertices += [x_range[0]*conv_factor, y_range[0]*conv_factor, z_range[0]*conv_factor]
 
         return Mesh([volume, mesh])
 
@@ -140,7 +121,6 @@ class MeshService(Service):
             raise ValueError("{} is not a valid voxel unit".format(voxel_unit))
         else:
             return voxel_unit.value
-
 
 class Mesh:
     def __init__(self, data):
@@ -163,7 +143,7 @@ class Mesh:
 
         """
         return self._mesh.to_precomputed()
-
+        
     def obj_mesh(self):
         """Convert mesh to obj
 