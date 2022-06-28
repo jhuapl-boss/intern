d6fa605681f8a2ac74f5199081cb265d996412dc
Jordan Matelsky <j6k4m8@gmail.com>
2022-06-28 17:30:39 -0400

    Fix tests from latest coordframe updates

--- intern/convenience/tests/test_remote_inferral.py
+++ intern/convenience/tests/test_remote_inferral.py
@@ -7,7 +7,7 @@ from intern import array
 class TestRemoteInferral(unittest.TestCase):
     def test_get_remote_type(self):
         data = array("bossdb://kasthuri2015/em/3cylneuron_v1")
-        self.assertEqual(data.remote, "BossDB")
+        self.assertEqual(data.remote, "CloudVolumeOpenData")
 
     def test_precompute_inference_cvdb(self):
         data = array(
